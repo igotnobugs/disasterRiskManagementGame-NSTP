676c4a8a139cae2efda3c6740487e534b76f8947
igotnobugs <jerrytabalongjr@mail.com>
2020-02-28 07:55:03 +0800

    Initial commit

---
+++ Assets/Scripts/ClickMoveScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMoveScript : MonoBehaviour {
+
+    public bool isSelected;
+    public Vector3 moveToPosition;
+    public GameObject gameObject;
+
+    // Use this for initialization
+    void Start () {
+        isSelected = false;
+        moveToPosition = this.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isSelected) {
+
+            if (Input.GetMouseButtonDown(0)) {
+                moveToPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                moveToPosition.y = this.gameObject.transform.position.y;
+            }
+
+            this.gameObject.transform.position = moveToPosition;
+            
+        }
+        Debug.Log(this.gameObject.transform.position + " " + moveToPosition);
+    }
+
+    //object is clicked
+    void OnMouseDown() {
+        isSelected = true;
+    }
+}
